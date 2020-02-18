282af5d720ca3db49a83bd22d74a83dc4ee81bf0
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-02-18 16:03:32 -0500

    POST create user

--- server/api/v1/users/controller.js
+++ server/api/v1/users/controller.js
@@ -1,7 +1,58 @@
 const users = [];
 
+function currentDate() {
+  const d = new Date();
+  return `${d.getFullYear()}-${d.getMonth()}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+}
+
 exports.create = (req, res, next) => {
-  res.json(users);
+  const { username = '', password = '', confirmPassword } = req.body;
+
+  if (username === '') {
+    next({
+      message: 'Param "username" is required.',
+      statusCode: 422,
+      type: 'warn',
+    });
+  } else if (password === '') {
+    next({
+      message: 'Param "password" is required.',
+      statusCode: 422,
+      type: 'warn',
+    });
+  } else if (confirmPassword === '') {
+    next({
+      message: 'Param "confirmPassword" is required.',
+      statusCode: 422,
+      type: 'warn',
+    });
+  } else if (password !== confirmPassword) {
+    next({
+      message: 'Params "password" and confirmPassword must match.',
+      statusCode: 422,
+      type: 'warn',
+    });
+  } else if (users.find(u => u.username === username)) {
+    next({
+      message: `"username" (${username}) is already taken.`,
+      statusCode: 422,
+      type: 'warn',
+    });
+  } else {
+    const user = {
+      id: users.length + 1,
+      username,
+      password,
+      createdAt: currentDate(),
+      updatedAt: null,
+    };
+
+    users.push(user);
+    res.statusCode(201);
+    res.json({
+      message: 'User created',
+    });
+  }
 };
 
 exports.all = (req, res, next) => {
