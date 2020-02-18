c6931ff020ef750e6824ba054899cca65f3e7f84
Jose Alejandro Rojano Crisson <jrojano@lean-tech.io>
2020-02-18 16:22:08 -0500

    PUT User update by Id

--- server/api/v1/users/controller.js
+++ server/api/v1/users/controller.js
@@ -74,7 +74,59 @@ exports.read = (req, res, next) => {
 };
 
 exports.update = (req, res, next) => {
-  res.json({});
+  const userIndex = users.findIndex(u => u.id === +req.params.id);
+  const user = users[userIndex];
+
+  if (userIndex >= 0) {
+    next({
+      message: `User (${req.params.id}) not found`,
+      statusCode: 404,
+      type: 'warn',
+    });
+  } else {
+    const { password = '', confirmPassword = '', oldPassword = '' } = req.body;
+
+    if (password === '') {
+      next({
+        message: 'Param "password" is required.',
+        statusCode: 422,
+        type: 'warn',
+      });
+    } else if (confirmPassword === '') {
+      next({
+        message: 'Param "confirmPassword" is required.',
+        statusCode: 422,
+        type: 'warn',
+      });
+    } else if (confirmPassword === '') {
+      next({
+        message: 'Param "oldPassword" is required.',
+        statusCode: 422,
+        type: 'warn',
+      });
+    } else if (oldPassword !== user.password) {
+      next({
+        message: 'Params "oldPassword" does not match the user\'s current password.',
+        statusCode: 422,
+        type: 'warn',
+      });
+    } else if (password !== confirmPassword) {
+      next({
+        message: 'Params "password" and confirmPassword must match.',
+        statusCode: 422,
+        type: 'warn',
+      });
+    }
+
+    user.password = password;
+    user.updatedAt = currentDate();
+
+    users.splice(userIndex, 1, user);
+
+    res.json({
+      message: `User (${user.id}) updated.`,
+    });
+  }
 };
 
 exports.delete = (req, res, next) => {
